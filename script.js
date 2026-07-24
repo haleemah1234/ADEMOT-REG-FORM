98d4071914daaff2db12c9d469503739c30b9c7f
haleemah omotunrayo <omotunrayoalimot8@gmail.com>
2026-07-24 09:11:50 -1100

    fix whatsapp form

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("regform").addEventListener(
         let phone = "2347061572871";
         let message = "As-salamualeikum warahmatullahi wabarahkatu. \n\nMy name is " + name + ".I am interested in joining your online computer training class.";
         
-        let url = "https://wa.me/" + phone + "?text =" + encodeURIComponent(message);
+        let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
         window.open(url, "_blank");
     });
     
