b3c5877ea52097a21ff587e54049e4a43ddac949
haleemah omotunrayo <omotunrayoalimot8@gmail.com>
2026-07-24 10:16:31 -1100

    commit

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.getElementById("regform").addEventListener(
         e.preventDefault();
         let name = document.getElementById("name").value;
         let phone = "2347061572871";
-        let message = "As-salamualeikum warahmatullahi wabarahkatu. \n\nMy name is " + name + ".I am interested in joining your online computer training class.";
+        // let message = "As-salamualeikum warahmatullahi wabarahkatu. \n\nMy name is " + name + ".I am interested in joining your online computer training class.";
         
-        let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
-        window.open(url, "_blank");
+        // let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+        window.open("https://wa.me/" + phone + "text=Hello%2OWorld", 
+            "_blank"
+        );
     });
     
